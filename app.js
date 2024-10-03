6f9815d04ec73a08f768b29af99de021f7f8afde
Muhammad Fazeel Raza <muhammadfazeel15it@gmail.com>
2024-10-03 19:53:09 +0500

    JavaScript Variable 1 Complete

---
+++ app.js
@@ -0,0 +1,26 @@
+var username="ABC";
+console.log(username);
+
+var myName="abc";
+console.log(myName);
+
+var message="Hello Word";
+console.log(message);
+
+alert ("Hello Word")
+
+alert ("ABC");
+alert ("15 years old");
+alert ("Web development");
+
+alert ("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+alert ("My email address is example@example.com");
+
+var book="A smarter way to learn JavaScript.";
+console.log(book);
+
+
+alert ("I am trying to learn from the Book A smarter way to learn JavaScript");
+
+document.write("Yah! I can write HTML content through JavaScript");
